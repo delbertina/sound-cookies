dfe9f77a583defb5a2920f7ae8ca3fb072a9a9c5
delbertina <6349928+delbertina@users.noreply.github.com>
2022-07-04 00:34:28 -0500

    Add test sound button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.scss';
 import React from 'react';
+import useSound from 'use-sound';
+import dundundun from './assets/sounds/dun-dun-dun.wav';
   
   
 class App extends React.Component {
@@ -26,12 +28,39 @@ class App extends React.Component {
           Woah that scroll button was money, baby.<br />
           This is where the actual content will go.
         </h6>
+        <SoundButton></SoundButton>
       </div>
     </div>
     );
   }
 
   executeScroll = () => this.myRef.current.scrollIntoView();
+}
+
+function SoundButton() {
+  // Make this dynamic in the future
+  const soundUrl = dundundun;
+
+  const [playbackRate, setPlaybackRate] = React.useState(0.75);
+
+  const [play] = useSound(soundUrl, {
+    playbackRate,
+    volume: 0.5,
+  });
+
+  const handleClick = () => {
+    console.log('clicked');
+    setPlaybackRate(playbackRate + 0.1);
+    play();
+  };
+
+  return (
+    <button onClick={handleClick}>
+      <span role="img" aria-label="Heart">
+      🎺
+      </span>
+    </button>
+  );
 }
 
 export default App;
