fed348866f9a9d2b43c86c36ad8cffaa0468460f
delbertina <6349928+delbertina@users.noreply.github.com>
2023-03-24 15:30:25 -0500

    Refactor pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
 import "./App.scss";
 import React from "react";
 import sounds from "./assets/sounds/sounds.json";
-import SoundList from "./components/SoundList/SoundList";
-import { Box, Button, IconButton, SwipeableDrawer } from "@mui/material";
-import SoundFilterBar from "./components/SoundFilterBar/SoundFilterBar";
-import { TagActionData, SoundData, SortEmojis } from "./types/sound-types";
-import SoundSortBar from "./components/SoundSortBar/SoundSortBar";
-import SoundSelectBar from "./components/SoundSelectBar/SoundSelectBar";
-import { grey } from "@mui/material/colors";
-import SettingsIcon from '@mui/icons-material/Settings';
+import { Button } from "@mui/material";
+import { SoundData } from "./types/sound-types";
+import MainPage from "./components/MainPage/MainPage";
 
 interface AppProps {}
 
-interface AppState {
-  sortData: TagActionData[];
-  sortDirection: boolean;
-  filterData: TagActionData[];
-  selectData: SoundData[];
-  soundData: SoundData[];
-  isSorting: boolean;
-  isFiltering: boolean;
-  isSelecting: boolean;
-  isSettingsOpen: boolean;
-}
+interface AppState {}
 
 class App extends React.Component<AppProps, AppState> {
   public myRef: React.RefObject<any>;
@@ -31,140 +16,6 @@ class App extends React.Component<AppProps, AppState> {
   constructor(props: any) {
     super(props);
     this.myRef = React.createRef();
-    const uniqueTags = (sounds as SoundData[])
-      .map((item) => item.tags)
-      .reduce((accumulator, value) => accumulator.concat(value), [])
-      .filter((value, index, array) => array.indexOf(value) === index)
-      .map((value) => ({
-        tag: value,
-        tagSelected: false,
-      }));
-    this.state = {
-      soundData: sounds as SoundData[],
-      filterData: uniqueTags.sort((a, b) =>
-        a.tag.localeCompare(b.tag, "en", { sensitivity: "base" })
-      ),
-      sortData: Object.keys(SortEmojis).map((item, i) => ({
-        tag: item,
-        tagSelected: i === 0,
-      })),
-      selectData: [],
-      sortDirection: true,
-      isSorting: true,
-      isFiltering: true,
-      isSelecting: false,
-      isSettingsOpen: false,
-    };
-    this.updateSoundList();
-  }
-
-  handleToggleSettings(): void {
-    this.setState({ isSettingsOpen: !this.state.isSettingsOpen });
-  }
-
-  handleSortDirectionSelect(isASC: boolean): void {
-    this.setState({ sortDirection: isASC }, this.updateSoundList);
-  }
-
-  handleSortTagSelect(index: number): void {
-    if (index < 0 || index > this.state.filterData.length - 1) return;
-
-    const sortData = this.state.sortData.map((sortItem, i) =>
-      index === i
-        ? {
-            tag: sortItem.tag,
-            tagSelected: true,
-          }
-        : {
-            tag: sortItem.tag,
-            tagSelected: false,
-          }
-    );
-
-    this.setState({ sortData }, this.updateSoundList);
-  }
-
-  handleFilterSelect(index: number): void {
-    if (index < 0 || index > this.state.filterData.length - 1) return;
-
-    const filterData = this.state.filterData.map((filter, i) =>
-      index === i
-        ? {
-            tag: filter.tag,
-            tagSelected: !filter.tagSelected,
-          }
-        : filter
-    );
-
-    this.setState({ filterData }, this.updateSoundList);
-  }
-
-  updateSoundList(): void {
-    const selectedFilters = this.state.filterData.filter(
-      (filter) => !!filter.tagSelected
-    );
-    const selectedSort = this.state.sortData.find(
-      (sortItem) => sortItem.tagSelected
-    );
-
-    let returnSounds: SoundData[] = [];
-
-    if (!selectedFilters.length) {
-      returnSounds = sounds as SoundData[];
-    } else {
-      // Only looks for 1 match, future work to switch to AND instead of OR
-      returnSounds = (sounds as SoundData[]).filter((sound) =>
-        selectedFilters
-          .map((filter) => filter.tag)
-          .some((tag) => sound.tags.indexOf(tag) >= 0)
-      );
-    }
-
-    if (selectedSort?.tag === "NAME") {
-      returnSounds.sort((a, b) =>
-        a.name.localeCompare(b.name, "en", { sensitivity: "base" })
-      );
-    } else if (selectedSort?.tag === "WHO") {
-      returnSounds.sort((a, b) =>
-        (a.tags[0] ?? "").localeCompare(b.tags[0] ?? "", "en", {
-          sensitivity: "base",
-        })
-      );
-    } else if (selectedSort?.tag === "DURATION") {
-      returnSounds.sort((a, b) => a.duration - b.duration);
-    }
-
-    if (!this.state.sortDirection) returnSounds.reverse();
-
-    this.setState({ soundData: returnSounds });
-  }
-
-  toggleSort(): void {
-    this.setState({ isSorting: !this.state.isSorting});
-  }
-
-  toggleFilter(): void {
-    this.setState({ isFiltering: !this.state.isFiltering});
-  }
-
-  toggleSelect(): void {
-    this.setState({ isSelecting: !this.state.isSelecting });
-  }
-
-  handleSoundItemDeselect(index: number): void {
-    if (index < 0 || index > this.state.selectData.length - 1) return;
-
-    this.setState({
-      selectData: this.state.selectData.slice().splice(index, 1),
-    });
-  }
-
-  handleSoundItemSelect(index: number): void {
-    if (index < 0 || index > this.state.soundData.length - 1) return;
-
-    this.setState({
-      selectData: [...this.state.selectData, this.state.soundData[index]],
-    });
   }
 
   render() {
@@ -180,50 +31,7 @@ class App extends React.Component<AppProps, AppState> {
           </Button>
         </header>
         <div className="App-body" ref={this.myRef}>
-          <SwipeableDrawer
-            anchor={'top'}
-            open={this.state.isSettingsOpen}
-            onClose={() => this.handleToggleSettings()}
-            onOpen={() => this.handleToggleSettings()}
-          >
-            <Box sx={{backgroundColor: grey[800], padding: 2, display: 'flex', justifyContent: 'center', gap: '16px'}}>
-              <Button variant='contained' color={this.state.isSorting ? 'info' : 'error'} onClick={() => this.toggleSort()}>Sort</Button>
-              <Button variant='contained' color={this.state.isFiltering ? 'info' : 'error'} onClick={() => this.toggleFilter()}>Filter</Button>
-              <Button variant='contained' color={this.state.isSelecting ? 'info' : 'error'} onClick={() => this.toggleSelect()}>Select</Button>
-            </Box>
-          </SwipeableDrawer>
-          <div>
-            <IconButton aria-label="settings" color="warning" onClick={() => this.handleToggleSettings()}>
-              <SettingsIcon />
-            </IconButton>
-          </div>
-          {this.state.isSorting &&
-          <SoundSortBar
-            sortData={this.state.sortData}
-            sortDirection={this.state.sortDirection}
-            sortClicked={(index: number) => this.handleSortTagSelect(index)}
-            sortDirectionClick={(isASC: boolean) =>
-              this.handleSortDirectionSelect(isASC)
-            }
-          />}
-          {this.state.isFiltering &&
-          <SoundFilterBar
-            filterData={this.state.filterData}
-            filterClicked={(index: number) => this.handleFilterSelect(index)}
-          />}
-          {this.state.isSelecting && 
-          <SoundSelectBar
-            selectData={this.state.selectData}
-            selectClicked={(index: number) =>
-              this.handleSoundItemDeselect(index)
-            }
-          />}
-          <SoundList
-            soundData={this.state.soundData}
-            isSelecting={this.state.isSelecting}
-            soundSelected={(index) => this.handleSoundItemSelect(index)}
-          />
-          <div className="App-body-dummy"></div>
+          <MainPage sounds={sounds as SoundData[]} />
         </div>
       </div>
     );
---
+++ src/components/MainPage/MainPage.tsx
@@ -0,0 +1,252 @@
+import "./MainPage.scss";
+import React from "react";
+import { Box, Button, IconButton, SwipeableDrawer } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import SettingsIcon from "@mui/icons-material/Settings";
+import { SortEmojis, SoundData, TagActionData } from "../../types/sound-types";
+import SoundSortBar from "../SoundSortBar/SoundSortBar";
+import SoundFilterBar from "../SoundFilterBar/SoundFilterBar";
+import SoundSelectBar from "../SoundSelectBar/SoundSelectBar";
+import SoundList from "../SoundList/SoundList";
+
+interface MainPageProps {
+  sounds: SoundData[];
+}
+
+interface MainPageState {
+  sortData: TagActionData[];
+  sortDirection: boolean;
+  filterData: TagActionData[];
+  selectData: SoundData[];
+  soundData: SoundData[];
+  isSorting: boolean;
+  isFiltering: boolean;
+  isSelecting: boolean;
+  isSettingsOpen: boolean;
+}
+
+class MainPage extends React.Component<MainPageProps, MainPageState> {
+  constructor(props: any) {
+    super(props);
+    const uniqueTags = this.props.sounds
+      .map((item) => item.tags)
+      .reduce((accumulator, value) => accumulator.concat(value), [])
+      .filter((value, index, array) => array.indexOf(value) === index)
+      .map((value) => ({
+        tag: value,
+        tagSelected: false,
+      }));
+    this.state = {
+      soundData: this.props.sounds,
+      filterData: uniqueTags.sort((a, b) =>
+        a.tag.localeCompare(b.tag, "en", { sensitivity: "base" })
+      ),
+      sortData: Object.keys(SortEmojis).map((item, i) => ({
+        tag: item,
+        tagSelected: i === 0,
+      })),
+      selectData: [],
+      sortDirection: true,
+      isSorting: true,
+      isFiltering: true,
+      isSelecting: false,
+      isSettingsOpen: false,
+    };
+    this.updateSoundList();
+  }
+
+  handleToggleSettings(): void {
+    this.setState({ isSettingsOpen: !this.state.isSettingsOpen });
+  }
+
+  handleSortDirectionSelect(isASC: boolean): void {
+    this.setState({ sortDirection: isASC }, this.updateSoundList);
+  }
+
+  handleSortTagSelect(index: number): void {
+    if (index < 0 || index > this.state.filterData.length - 1) return;
+
+    const sortData = this.state.sortData.map((sortItem, i) =>
+      index === i
+        ? {
+            tag: sortItem.tag,
+            tagSelected: true,
+          }
+        : {
+            tag: sortItem.tag,
+            tagSelected: false,
+          }
+    );
+
+    this.setState({ sortData }, this.updateSoundList);
+  }
+
+  handleFilterSelect(index: number): void {
+    if (index < 0 || index > this.state.filterData.length - 1) return;
+
+    const filterData = this.state.filterData.map((filter, i) =>
+      index === i
+        ? {
+            tag: filter.tag,
+            tagSelected: !filter.tagSelected,
+          }
+        : filter
+    );
+
+    this.setState({ filterData }, this.updateSoundList);
+  }
+
+  updateSoundList(): void {
+    const selectedFilters = this.state.filterData.filter(
+      (filter) => !!filter.tagSelected
+    );
+    const selectedSort = this.state.sortData.find(
+      (sortItem) => sortItem.tagSelected
+    );
+
+    let returnSounds: SoundData[] = [];
+
+    if (!selectedFilters.length) {
+      returnSounds = this.props.sounds;
+    } else {
+      // Only looks for 1 match, future work to switch to AND instead of OR
+      returnSounds = this.props.sounds.filter((sound) =>
+        selectedFilters
+          .map((filter) => filter.tag)
+          .some((tag) => sound.tags.indexOf(tag) >= 0)
+      );
+    }
+
+    if (selectedSort?.tag === "NAME") {
+      returnSounds.sort((a, b) =>
+        a.name.localeCompare(b.name, "en", { sensitivity: "base" })
+      );
+    } else if (selectedSort?.tag === "WHO") {
+      returnSounds.sort((a, b) =>
+        (a.tags[0] ?? "").localeCompare(b.tags[0] ?? "", "en", {
+          sensitivity: "base",
+        })
+      );
+    } else if (selectedSort?.tag === "DURATION") {
+      returnSounds.sort((a, b) => a.duration - b.duration);
+    }
+
+    if (!this.state.sortDirection) returnSounds.reverse();
+
+    this.setState({ soundData: returnSounds });
+  }
+
+  toggleSort(): void {
+    this.setState({ isSorting: !this.state.isSorting });
+  }
+
+  toggleFilter(): void {
+    this.setState({ isFiltering: !this.state.isFiltering });
+  }
+
+  toggleSelect(): void {
+    this.setState({ isSelecting: !this.state.isSelecting });
+  }
+
+  handleSoundItemDeselect(index: number): void {
+    if (index < 0 || index > this.state.selectData.length - 1) return;
+
+    this.setState({
+      selectData: this.state.selectData.slice().splice(index, 1),
+    });
+  }
+
+  handleSoundItemSelect(index: number): void {
+    if (index < 0 || index > this.state.soundData.length - 1) return;
+
+    this.setState({
+      selectData: [...this.state.selectData, this.state.soundData[index]],
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <SwipeableDrawer
+          anchor={"top"}
+          open={this.state.isSettingsOpen}
+          onClose={() => this.handleToggleSettings()}
+          onOpen={() => this.handleToggleSettings()}
+        >
+          <Box
+            sx={{
+              backgroundColor: grey[800],
+              padding: 2,
+              display: "flex",
+              justifyContent: "center",
+              gap: "16px",
+            }}
+          >
+            <Button
+              variant="contained"
+              color={this.state.isSorting ? "info" : "error"}
+              onClick={() => this.toggleSort()}
+            >
+              Sort
+            </Button>
+            <Button
+              variant="contained"
+              color={this.state.isFiltering ? "info" : "error"}
+              onClick={() => this.toggleFilter()}
+            >
+              Filter
+            </Button>
+            <Button
+              variant="contained"
+              color={this.state.isSelecting ? "info" : "error"}
+              onClick={() => this.toggleSelect()}
+            >
+              Select
+            </Button>
+          </Box>
+        </SwipeableDrawer>
+        <div>
+          <IconButton
+            aria-label="settings"
+            color="warning"
+            onClick={() => this.handleToggleSettings()}
+          >
+            <SettingsIcon />
+          </IconButton>
+        </div>
+        {this.state.isSorting && (
+          <SoundSortBar
+            sortData={this.state.sortData}
+            sortDirection={this.state.sortDirection}
+            sortClicked={(index: number) => this.handleSortTagSelect(index)}
+            sortDirectionClick={(isASC: boolean) =>
+              this.handleSortDirectionSelect(isASC)
+            }
+          />
+        )}
+        {this.state.isFiltering && (
+          <SoundFilterBar
+            filterData={this.state.filterData}
+            filterClicked={(index: number) => this.handleFilterSelect(index)}
+          />
+        )}
+        {this.state.isSelecting && (
+          <SoundSelectBar
+            selectData={this.state.selectData}
+            selectClicked={(index: number) =>
+              this.handleSoundItemDeselect(index)
+            }
+          />
+        )}
+        <SoundList
+          soundData={this.state.soundData}
+          isSelecting={this.state.isSelecting}
+          soundSelected={(index) => this.handleSoundItemSelect(index)}
+        />
+        <div className="main-page-dummy"></div>
+      </>
+    );
+  }
+}
+
+export default MainPage;
